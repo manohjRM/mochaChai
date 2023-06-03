ee86b9a7bd1e79f8d4f6ab1f6e1482d5615dcc18
Softworks Technologies <mmanian@lenovo.com>
2023-06-03 21:27:54 +0530

    all

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,8 +16,8 @@ suite('Functional Tests', function () {
         .keepOpen()
         .get('/hello')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello Guest');
+          assert.equal(res.status, 200, 'Response status should be 200');
+          assert.equal(res.text, 'hello Guest', 'Response should be "hello Guest"');
           done();
         });
     });
@@ -26,10 +26,10 @@ suite('Functional Tests', function () {
       chai
         .request(server)
         .keepOpen()
-        .get('/hello?name=xy_z')
+        .get('/hello?name=Manoj')
         .end(function (err, res) {
-          assert.fail(res.status, 200);
-          assert.fail(res.text, 'hello xy_z');
+          assert.equal(res.status, 200, 'Response status should be 200');
+          assert.equal(res.text, 'hello Manoj', 'Response should be "hello Manoj"');
           done();
         });
     });
@@ -39,28 +39,45 @@ suite('Functional Tests', function () {
         .request(server)
         .keepOpen()
         .put('/travellers')
-
+        .send({
+          "surname": "Colombo"
+        })
         .end(function (err, res) {
-          assert.fail();
-
+          assert.equal(res.status, 200, 'Response should be 200');
+          assert.equal(res.type, 'application/json', "Response should be 'application/json'");
+          assert.equal(res.body.name, 'Cristoforo', 'Response name should be Cristoforo');
+          assert.equal(res.body.surname, 'Colombo', 'Response surname should have Colombo')
           done();
         });
     });
     // #4
     test('Send {surname: "da Verrazzano"}', function (done) {
-      assert.fail();
-
-      done();
+      chai
+        .request(server)
+        .keepOpen()
+        .put('/travellers')
+        .send({
+          "surname": "da Verrazzano"
+        })
+        .end(function (err, res) {
+          assert.equal(res.status, 200, 'Response should be 200');
+          assert.equal(res.type, 'application/json', "Response should be 'application/json'");
+          assert.equal(res.body.name, 'Giovanni', 'Response name should be Giovanni');
+          assert.equal(res.body.surname, 'da Verrazzano', 'Response surname should have da Verrazzano')
+          done();
+        });
     });
   });
 });
 
 const Browser = require('zombie');
-
+Browser.site = 'https://trusting-berry-quokka.glitch.me';
 suite('Functional Tests with Zombie.js', function () {
   this.timeout(5000);
-
-
+  const browser = new Browser();
+  suiteSetup(function(done) {
+    return browser.visit('/', done);
+  });
 
   suite('Headless browser', function () {
     test('should have a working "site" property', function() {
@@ -71,15 +88,27 @@ suite('Functional Tests with Zombie.js', function () {
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Colombo').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Cristoforo');
+          browser.assert.text('span#surname', 'Colombo');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
     // #6
     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-      assert.fail();
-
-      done();
+      browser.fill('surname', 'Vespucci').then(() => {
+        browser.pressButton('submit', () => {
+          browser.assert.success();
+          browser.assert.text('span#name', 'Amerigo');
+          browser.assert.text('span#surname', 'Vespucci');
+          browser.assert.elements('span#dates', 1);
+          done();
+        });
+      });
     });
   });
 });
